86ec2b1f9cabae49cf63372fb2dec8b031e38ef1
Yobubble <thanachot.onl@student.mahidol.edu>
2024-03-27 17:45:53 +0700

    fix : WEB-33 no single quote

--- src/routes/course.js
+++ src/routes/course.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const pool = require('../configuration/database');
 
 /// Enable BodyParser
-router0.use(express.json());
-router0.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 // Add routes as needed
 // SAMPLE CODE
 // router.get('', (req, res) => {
-//     pool.query('SELECT * FROM table', (error, results) => {
+//     pool.query('SELECT * FROM Courses', (error, results) => {
 //       if (error) {
 //         console.error(error);
 //         return res.status(500).json({ message: 'Internal Server Error' });
@@ -19,9 +19,10 @@ router0.use(express.urlencoded({ extended: true }));
 //   });
 
 router.post('', (req,res) => {
-    const { courseId, courseCode, courseCat, courseName, courseDes, courseDuration, price, TeacherId, rating, status } = req.body
-    pool.query(`INSERT INTO <courseTable> VALUES (${courseId}, ${courseCode}, ${courseCat}, ${courseName}, ${courseDes}, ${courseDuration}, ${TeacherId}, ${price}, ${rating}, ${status})`, (error, results) => {
+    const { courseCode, courseCat, courseName, courseDes, courseDuration, price, TeacherId, rating, status } = req.body
+    pool.query(`INSERT INTO Courses (courseCode, courseCat, courseName, courseDes, courseDuration, price, TeacherId, rating, status) VALUES ('${courseCode}', '${courseCat}', '${courseName}', '${courseDes}', ${courseDuration}, ${price},${TeacherId}, ${rating}, ${status})`, (error, results) => {
         if (error) {
+            console.log(error)
             return res.status(500).json({
                 message : "Missing Required Field(s)"
             })
