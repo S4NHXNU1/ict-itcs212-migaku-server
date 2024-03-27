8beb7790259c1ed3ed9817a5d11eb605b4cc1c2c
Yobubble <thanachot.onl@student.mahidol.edu>
2024-03-27 17:05:52 +0700

    feat : WEB-37  update user

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,4 +14,20 @@ const pool = require('../configuration/database');
 //     });
 //   });
 
+router.put('/:userId', (req,res) => {
+    const id = req.params.userId
+    const { FirstName, LastName, Email, username, password, role } = req.body
+    pool.query(`UPDATE <userTable> SET FirstName = ${FirstName}, LastName = ${LastName}, Email = ${Email}, username = ${username}, password = ${password}, role = ${role} WHERE userId = ${id}`,(error, results) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ 
+                message: 'Missing Required Field(s)' 
+            });
+        }
+        res.status(200).json({
+            message : "User Updated"
+        })
+    })
+})
+
 module.exports = router;
