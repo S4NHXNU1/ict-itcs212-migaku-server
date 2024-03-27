b238ff264dcc51b8bb20cfd589f50739e83ff65f
Yobubble <107536925+Yoboba@users.noreply.github.com>
2024-03-27 16:51:35 +0700

    feat : WEB-33 post course
    waiting for test with db

--- src/routes/course.js
+++ src/routes/course.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../configuration/database');
 
+/// Enable BodyParser
+router0.use(express.json());
+router0.use(express.urlencoded({ extended: true }));
+
 // Add routes as needed
 // SAMPLE CODE
 // router.get('', (req, res) => {
@@ -13,5 +17,19 @@ const pool = require('../configuration/database');
 //       res.json(results);
 //     });
 //   });
+
+router.post('', (req,res) => {
+    const { courseId, courseCode, courseCat, courseName, courseDes, courseDuration, price, TeacherId, rating, status} = req.body
+    pool.query(`insert into <tablerName> values (${courseId}, ${courseCode}, ${courseCat}, ${courseName}, ${courseDes}, ${courseDuration}, ${TeacherId}, ${price}, ${rating}, ${status})`, (error, results) => {
+        if (error) {
+            return res.status(500).json({
+                message : "Missing Required Field(s)"
+            })
+        }
+        res.status(200).json({
+            message : "Course Created"
+        })
+    })
+})
 
 module.exports = router;
