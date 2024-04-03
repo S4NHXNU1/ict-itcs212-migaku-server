e185d69341e8c0d7375473d2637034a5a5e9abe5
S4NHXNU1 <sanhanut2003@gmail.com>
2024-04-03 18:06:26 +0700

    feat: WEB-61 add cookie parser

--- src/configuration/setup.js
+++ src/configuration/setup.js
@@ -3,10 +3,12 @@ const middleware = require('./middleware');
 const AuthRoutes = require('../routes/auth');
 const UserRoutes = require('../routes/user');
 const CourseRoutes = require('../routes/course');
+const cookieParser = require('cookie-parser');
 
 function setup(app)
 {
     app.use(express.json());
+    app.use(cookieParser());
     app.use(middleware);
     app.use('/api/auth', AuthRoutes);
     app.use('/api/user', UserRoutes);
