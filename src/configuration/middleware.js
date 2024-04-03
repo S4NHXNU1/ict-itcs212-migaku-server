9bf9aafaa55b5c6ef452956117ed138d0e47b053
S4NHXNU1 <sanhanut2003@gmail.com>
2024-04-03 18:06:50 +0700

    feat: WEB-61 add logout logic

--- src/configuration/middleware.js
+++ src/configuration/middleware.js
@@ -2,10 +2,10 @@ const pool = require('../configuration/database');
 
 function middleware(req, res, next) {
     // Middleware logics go here
-    if(req.url === '/api/auth') next();
+    if(req.url === '/api/auth' || req.url === '/api/auth/logout') next();
     else
     {
-        console.log('Custom Middleware');
+        //console.log('Custom Middleware');
         if(req.headers === undefined || req.headers.authorization === "" || req.headers.authorization === undefined)
             return res.status(400).json({ Message: "There’s no authorization header attached" });
         const userId = req.headers.authorization;
@@ -16,7 +16,7 @@ function middleware(req, res, next) {
             }
             if(results && results.length > 0)
             {
-                console.log(results)
+                //console.log(results)
                 if(results[0].userId === Number(userId)) next();
                 else return res.status(403).json({ Message: "Invalid User Id" });
             }
