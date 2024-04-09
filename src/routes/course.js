acbb486fcc82a82824b3ce6ac9fc407f755eef13
DAVEPL <107537174+DEVN-P@users.noreply.github.com>
2024-04-09 16:53:01 +0700

    feature: get course at /browse

--- src/routes/course.js
+++ src/routes/course.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../configuration/database');
 const isUndefined = require('../utils/isUndefined');
+const isEmpty = require('../utils/isEmpty')
 
 /// Enable BodyParser
 router.use(express.json());
@@ -19,10 +20,68 @@ router.use(express.urlencoded({ extended: true }));
 //     });
 //   });
 
+router.get('/browse', (req, res) => {
+    if (isUndefined(req.body)) {
+        return res.status(400).json({
+            Message: "Missing request body"
+        })
+    }
+
+    searchKey = req.body?.searchKey
+    courseCat = req.body?.courseCat
+    teacherName = req.body?.name
+
+    if (isUndefined(searchKey) || isUndefined(courseCat) || isUndefined(teacherName)) {
+        return res.status(400).json({
+            Message: "Bad request"
+        })
+    }
+
+    query = `SELECT c.courseId, c.courseCode, c.courseCat, c.courseName, c.courseDes, c.courseDuration, c.price, c.status, c.rating 
+    FROM Courses c INNER JOIN Users u ON c.teacherId = u.userId`
+    
+    if (!isEmpty(searchKey) || !isEmpty(teacherName) || courseCat !== 'All') {
+        query += ' WHERE '
+        conditionsArr = []
+        if (!isEmpty(searchKey)) {
+            conditionsArr.push(`c.courseName LIKE '%${searchKey}%'`)
+        }
+        if (courseCat !== 'All') {
+            conditionsArr.push(`c.courseCat = '${courseCat}'`)
+        }
+        if (!isEmpty(teacherName)) {
+            conditionsArr.push(`CONCAT(u.firstName, " ", u.lastName) LIKE '%${teacherName}%'`)
+        }
+        query += conditionsArr.join(" AND ")
+    }
+    
+    //console.log(query)
+
+    pool.query(query, (error, result) => {
+        if (error) {
+            console.log(error)
+            return res.status(500).json({
+                Message : "Internal Server Error"
+            })
+        }
+
+        if (result) {
+            return res.status(200).json(result)
+        }
+    })
+})
+
 router.post('', (req,res) => {
+
+    if (isUndefined(req.body) || req.body === "")
+        return res.status(400).json({Message: "Missing Required Field(s)"})
+
     const { courseCode, courseCat, courseName, courseDes, courseDuration, price, TeacherId, rating, status } = req.body;
-    if(req.body === "" || courseCode === "" || courseCat === "" || courseName === "" || courseDes === "" || courseDuration === "" ||
-    price === "" || TeacherId === "" || rating === "" || status === "") return res.status(400).json({Message: "Missing Required Field(s)"})
+
+    if(courseCode === "" || courseCat === "" || courseName === "" || courseDes === "" || courseDuration === "" ||
+    price === "" || TeacherId === "" || rating === "" || status === "")
+        return res.status(400).json({Message: "Missing Required Field(s)"})
+    
     pool.query(`INSERT INTO Courses (courseCode, courseCat, courseName, courseDes, courseDuration, price, TeacherId, rating, status)
     VALUES ('${courseCode}', '${courseCat}', '${courseName}', '${courseDes}', '${courseDuration}', '${price}', '${TeacherId}', '${rating}', ${status})`, (error, results) => {
         if (error) {
@@ -40,7 +99,7 @@ router.post('', (req,res) => {
 router.delete('', (req,res) => {
 
     if(isUndefined(req.query) || req.query === "" || isUndefined(req.query.courseId) || req.query.courseId === "")
-    return res.status(400).json({Message: "Missing Required Field"});
+        return res.status(400).json({Message: "Missing Required Field"});
 
     const courseId = req.query.courseId;
     pool.query(`DELETE FROM Courses WHERE courseId = ${courseId}`, (error,results) => {
